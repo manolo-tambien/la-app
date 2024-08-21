690293ceff8718dd58cb98235d4c8aa6fc101ec5
Manuel Gonzalez <magpags@gmail.com>
2024-08-21 14:26:19 -0600

    Se eliminan comentarios

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,13 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
-import { debug } from 'console';
-
 export async function middleware(request) {
   const token = request.cookies.get('userToken')?.value || null;
-
   try {
     if (token) {
-      // Convertir el secreto de JWT a Uint8Array
       const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-      // Verificar el token especificando el algoritmo de firma (por ejemplo, 'HS256')
       const { payload } = await jwtVerify(token, secret, {
         algorithms: ['HS256'],
       });
-
-      // Si el token es válido, permitir el acceso a la aplicación
       if(request.nextUrl.pathname.startsWith('/login')){
         return NextResponse.redirect(new URL('/', request.url));
       }else{
@@ -25,16 +17,11 @@ export async function middleware(request) {
   } catch (error) {
     console.error('JWT verification failed:', error);
   }
-
-  // Si no hay token válido, redirigir al usuario a la página de inicio de sesión
   if (!request.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
-  // Si la ruta es '/login', permitir el acceso
   return NextResponse.next();
 }
-
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
