e8037720e3064cacba50f5cb5b95b05b7a9b30f8
Manuel Gonzalez <magpags@gmail.com>
2024-08-21 13:26:53 -0600

    Se agrega validacion para redirigir
    Se agrega validacion para redireccionar la aplicacion cuando ya esté autenticado y se quiera entrar a la ruta de /login y que se redireccione a home automaticamente

--- src/middleware.js
+++ src/middleware.js
@@ -5,28 +5,33 @@ import { debug } from 'console';
 export async function middleware(request) {
   const token = request.cookies.get('userToken')?.value || null;
 
-  if (!token && !request.nextUrl.pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
   try {
     if (token) {
       // Convertir el secreto de JWT a Uint8Array
       const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
       // Verificar el token especificando el algoritmo de firma (por ejemplo, 'HS256')
       const { payload } = await jwtVerify(token, secret, {
         algorithms: ['HS256'],
       });
-      // Si el token es válido y la ruta es login, redirige al home
-      if (request.nextUrl.pathname === '/login') {
+
+      // Si el token es válido, permitir el acceso a la aplicación
+      if(request.nextUrl.pathname.startsWith('/login')){
         return NextResponse.redirect(new URL('/', request.url));
+      }else{
+        return NextResponse.next();
       }
     }
   } catch (error) {
     console.error('JWT verification failed:', error);
+  }
+
+  // Si no hay token válido, redirigir al usuario a la página de inicio de sesión
+  if (!request.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
+  // Si la ruta es '/login', permitir el acceso
   return NextResponse.next();
 }
 
